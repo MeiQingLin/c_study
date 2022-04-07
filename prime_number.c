83b5dd9833334e975a890fd3a0557c9ec4c8ca9e
梅清琳 <100503801+MeiQinglin@users.noreply.github.com>
2022-04-07 18:13:12 +0800

    打印200-500之间的素数

---
+++ prime_number.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int i = 0;
+	int j = 0;
+	int m = 0; // 用以统计一行打印多少个了
+	for (i = 201; i < 500; i += 2) {
+		int s = sqrt(i);
+		for (j = 2; j <= s; j++) {
+			if (i % j == 0) {
+				break;
+			}
+		}
+		if (j == s + 1) {
+			printf("%-6d", i);
+			m += 1;
+			if (m % 10 == 0) {
+				printf("\n");
+			}
+		}
+	}
+	return 0;
+}
